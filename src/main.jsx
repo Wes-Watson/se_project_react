bcf0d32b00ba847644c0be72215e9ab5ce1266da
Wes Watson <wesrwats@gmail.com>
2025-02-21 14:23:57 -0600

    Update favorites

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App.jsx";
 import "./index.css";
 
+//#5feb34
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
